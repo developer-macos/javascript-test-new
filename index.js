45cba12f6f46bffb042ed6846ccf582d75cd292c
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2025-02-12 20:30:42 +0100

    saa

---
+++ index.js
@@ -0,0 +1,20 @@
+let me = "vova";
+const age = 15;
+var myage = me + age;
+
+
+// const message = "Javascript is awesome!";
+
+// const extramessage = " Is it cool?";
+
+const isComing = confirm("Please confirm hotel reservation");
+
+const hotelName = prompt("Please enter desired hotel name");
+
+console.log(hotelName);
+
+
+// const lastName = "Hello " + name;
+// let name = prompt("What is your last name?")
+
+// console.log(lastName)
