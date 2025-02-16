2f883d71a05332c64d4922421858fda1f824d8bb
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2025-02-16 20:47:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,44 @@ let me = "vova";
 const age = 15;
 var myage = me + age;
 
+// x = 10
+// y = 20
+// z = 15
 
-// const message = "Javascript is awesome!";
-
-// const extramessage = " Is it cool?";
-
-const isComing = confirm("Please confirm hotel reservation");
-
-const hotelName = prompt("Please enter desired hotel name");
-
-console.log(hotelName);
+// console.log(x - y);
+// console.log(x - z);
+// console.log(y - z);
+// console.log(y - x);
 
 
-// const lastName = "Hello " + name;
-// let name = prompt("What is your last name?")
 
-// console.log(lastName)
+// const validNummer = Number("51");
+
+console.log((0.1 * 10 + 0.2 * 10) / 10);
+
+//  Math.floor(num) - повертає найбільше ціле число, менше, або дорівнює вказаному
+
+console.log(Math.floor(1.7)); // 1
+
+// Math.ceil(num) - повертає найменше ціле число,
+// більше, або дорівнює зазначеному числу.
+
+console.log(Math.ceil(1.2));
+
+
+// Math.round(num) - повертає значення числа, округлене до найближчого цілого
+
+console.log(Math.round(1.5, 2, 4, 5, 1, 6, 7)); // 2
+
+
+console.log(2 ** 4);
+
+
+console.log(Math.random() * (10 - 1) + 1);
+
+
+const x = Number(prompt("Введіть перше число:"));
+const y = Number(prompt("Введіть друге число:"));
+
+alert(x + " + " + y + " = " + (x * 22 + 222 / 3000000 + y + 2122 * 22 * 3000000));
+// console.log(validNummer)
