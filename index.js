ca1b18307c4c8a05f62a9ad4be1829cc2b2094d7
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2025-02-27 19:06:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,112 @@
-let me = "vova";
-const age = 15;
-var myage = me + age;
-
-// x = 10
-// y = 20
-// z = 15
-
-// console.log(x - y);
-// console.log(x - z);
-// console.log(y - z);
-// console.log(y - x);
 
 
 
-// const validNummer = Number("51");
 
-console.log((0.1 * 10 + 0.2 * 10) / 10);
+// // const validNummer = Number("51");
 
-//  Math.floor(num) - повертає найбільше ціле число, менше, або дорівнює вказаному
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
 
-console.log(Math.floor(1.7)); // 1
+// //  Math.floor(num) - повертає найбільше ціле число, менше, або дорівнює вказаному
 
-// Math.ceil(num) - повертає найменше ціле число,
-// більше, або дорівнює зазначеному числу.
+// console.log(Math.floor(1.7)); // 1
 
-console.log(Math.ceil(1.2));
+// // Math.ceil(num) - повертає найменше ціле число,
+// // більше, або дорівнює зазначеному числу.
 
-
-// Math.round(num) - повертає значення числа, округлене до найближчого цілого
-
-console.log(Math.round(1.5, 2, 4, 5, 1, 6, 7)); // 2
+// console.log(Math.ceil(1.2));
 
 
-console.log(2 ** 4);
+// // Math.round(num) - повертає значення числа, округлене до найближчого цілого
+
+// console.log(Math.round(1.5, 2, 4, 5, 1, 6, 7)); // 2
 
 
-console.log(Math.random() * (10 - 1) + 1);
+// console.log(2 ** 4);
 
 
-const x = Number(prompt("Введіть перше число:"));
-const y = Number(prompt("Введіть друге число:"));
+// console.log(Math.random() * (10 - 1) + 1);
 
-alert(x + " + " + y + " = " + (x * 22 + 222 / 3000000 + y + 2122 * 22 * 3000000));
+
+// // const x = Number(prompt("Введіть перше число:"));
+// // const y = Number(prompt("Введіть друге число:"));
+
+// alert(x + " + " + y + " = " + (x * 22 + 222 / 3000000 + y + 2122 * 22 * 3000000));
 // console.log(validNummer)
+
+
+// const message = 'Mango ' + 'is' + ' happy';
+// console.log(message)
+
+// let result;
+
+
+// result = 5 + '5';
+
+// result = 5 + '5' + 5;
+
+// console.log(result)
+
+// const message = 'Welcome to Bahamas!';
+
+
+// console.log(message.length);
+
+// console.log(message.toLowerCase());
+
+
+// console.log(message.toUpperCase());
+
+// console.log(message.indexOf('to'));
+
+// console.log(message.startsWith('Wel'));
+
+
+// console.log(message.endsWith('!'));
+
+// const name = 'Vorext AI';
+// const age = 'nAn';
+// const mood = 'noone';
+// const message =
+//     'My name is ' + name + ",I am " + age + ' years old and ' + mood + '.';
+// console.log(message);
+
+// const sameMessage = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+
+
+let username = "Vova Iarovyi";
+
+console.log(username.indexOf("a"));
+
+let x = "Norman";
+
+let y = "Alex";
+
+let n = 1;
+
+let g = 11;
+
+console.log("Гість", x, y, "поселяється в", n, "номер", g);
+
+
+const message = prompt('Type your message. No copying or instant ban from the group!');
+console.log(message.trim());
+// Vorext AI 2025 Copyright;
+// No copying or instant ban from the group!
+
+
+const orderMsg = "Ви замовляєте 15 тістечок.";
+console.log(orderMsg.includes(15));
+console.log(orderMsg.indexOf('1'));
+// Vorext AI 2025 Copyright;
+// No copying or instant ban from the group!
+
+
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'розпродаж';
+
+
+const string1 = 'Привіт, я Vortex AI, це не спам, пропоную тобі мільйон робаксів!';
+const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+const string3 = 'Рекламна кампанія #jsIsAwesome';
+// Vorext AI 2025 Copyright;
+// No copying or instant ban from the group!
